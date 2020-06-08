28e50eaf76628c585cb79bee61b491735e31fa0b
龙 张 <zhanglong@xiaoxiangyoupin.cn>
2020-06-08 09:51:17 +0800

    删除排序数组中的重复项

--- src/com/zl/leetcode/array/RemoveDuplicatesFromSortedArray_26.java
+++ src/com/zl/leetcode/array/RemoveDuplicatesFromSortedArray_26.java
@@ -1,14 +1,45 @@
 package com.zl.leetcode.array;
 
 /**
- * 删除数组中的重复项
+ * 删除排序数组中的重复项
  *
  * 不需要考虑数组中超出新长度的后面
  * 需要使用O(1)的空间复杂度
  */
 public class RemoveDuplicatesFromSortedArray_26 {
 
-    public int removeDuplicates(int[] nums) {
-        return -1;
+    /**
+     * 方法1：逐个遍历并赋值，遇到当前值后面有重复值的直接跳过
+     * @param nums
+     * @return
+     */
+    public int removeDuplicates1(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        int n = nums.length;
+        int idx = 0;
+        for (int i = 0; i < n; i++) {
+            if (i == n - 1 || nums[i] != nums[i + 1]) {
+                nums[idx++] = nums[i];
+            }
+        }
+        return idx;
+    }
+
+    /**
+     * 方法2：快慢指针
+     * @param nums
+     * @return
+     */
+    public int removeDuplicates2(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        int n = nums.length;
+        int slow = 0, fast = 1;
+        while (fast < n) {
+            if (nums[slow] != nums[fast]) {
+                nums[++slow] = nums[fast];
+            }
+            fast++;
+        }
+        return slow + 1;
     }
 }
---
+++ src/com/zl/leetcode/test/TestArray.java
@@ -0,0 +1,11 @@
+package com.zl.leetcode.test;
+
+/**
+ * Create by zhanglong on 2020/6/8
+ */
+public class TestArray {
+    public static void main(String[] args) {
+
+    }
+
+}
