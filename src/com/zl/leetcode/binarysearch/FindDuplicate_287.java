4d93232a06cfea99ae77408ab93aa3fb64e0f4e7
zhanglong <dravenzl@163.com>
2020-06-10 10:59:54 +0800

    二分查找重复的数

--- src/com/zl/leetcode/binarysearch/FindDuplicate_287.java
+++ src/com/zl/leetcode/binarysearch/FindDuplicate_287.java
@@ -6,46 +6,38 @@ import java.util.Arrays;
  * 287.寻找重复数
  * 数组中有n+1个数，取值范围都在[1,n]
  *
- * 不能更改原数组（假设数组是只读的）。
- * 只能使用额外的 O(1) 的空间。
- * 时间复杂度小于 O(n2) 。
+ * 不能更改原数组（假设数组是只读的）。  - 排除排序
+ * 只能使用额外的 O(1) 的空间。         - 排除hash表
+ * 时间复杂度小于 O(n2) 。             - 排除暴力法
  * 数组中只有一个重复的数字，但它可能不止重复出现一次。
  *
  * Create by zhanglong on 2020/6/9
  */
 public class FindDuplicate_287 {
 
-    /**
-     * 方法1：排序后遍历
-     * @param nums
-     * @return
-     */
-    public int findDuplicate1(int[] nums) {
-        Arrays.sort(nums);
-        for (int i = 0; i < nums.length - 1; i++) {
-            if (nums[i] == nums[i+1]) return nums[i];
-        }
-        return -1;
-    }
 
     /**
-     * 方法2：二分查找
+     * 方法1：二分查找
+     *
+     * 关键：与传统二分思维不同，最终找到的是值，而不是索引
      * TODO 回看
      * @param nums
      * @return
      */
-    public int findDuplicate2(int[] nums) {
-        int left = 0, right = nums.length;
+    public int findDuplicate1(int[] nums) {
+        // 一共nums.length个数，所以所有数的范围是[1, nums.length - 1]
+        int left = 1, right = nums.length - 1;
+        //
         while (left < right) {
             int mid = (left + right) >>> 1;
             int cnt = 0;
             for (int num : nums) {
                 if (num <= mid) cnt++;
             }
-            // 如果小于等于mid的数  小于等于mid个，考虑右侧区间
+            // 如果小于等于mid的数严格大于mid个，肯定在左侧区间
             if (cnt > mid) {
                 right = mid;
-            } else {
+            } else { // 否则在另一个区间查找
                 left = mid + 1;
             }
         }
--- src/com/zl/leetcode/test/TestArray.java
+++ src/com/zl/leetcode/test/TestArray.java
@@ -9,11 +9,13 @@ public class TestArray {
     }
 
     public int findDuplicate2(int[] nums) {
-        int left = 0, right = nums.length;
+        int left = 1, right = nums.length - 1;
         while (left < right) {
+            // 在[0, n)中先随便取一个值
             int mid = (left + right) >>> 1;
             int cnt = 0;
             for (int num : nums) {
+                // 统计出小于等于当前mid值的个数
                 if (num <= mid) cnt++;
             }
             if (cnt > mid) {
