79a03d1c2fb7c5650b906c7476f5a5e6ef4769d5
龙 张 <zhanglong@xiaoxiangyoupin.cn>
2020-07-27 16:07:19 +0800

    分治

--- src/com/zl/leetcode/recursion/UniqueBST2_95.java
+++ src/com/zl/leetcode/recursion/UniqueBST2_95.java
@@ -12,6 +12,7 @@ import java.util.List;
 public class UniqueBST2_95 {
 
     public List<TreeNode> generateTrees(int n) {
+        if (n == 0) return new ArrayList<>();
         return generateTrees(1, n);
     }
 
